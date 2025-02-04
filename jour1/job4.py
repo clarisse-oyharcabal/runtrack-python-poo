a674cb59bf55be7f79e2ff5b6f523b63b10bd6b7
clarisse-oyharcabal <clarisse.oyharcabal@laplateforme.io>
2025-02-04 13:07:24 +0100

    <doc>: add the final version of job4 exercice

--- jour1/job4.py
+++ jour1/job4.py
@@ -1,10 +1,33 @@
-#job4 
-class Personne : 
-    def __init__ (self, prenom, nom):
-        self.prenom = prenom
+class Personne:
+    # Constructeur de la classe
+    def __init__(self, nom, prenom):
         self.nom = nom
+        self.prenom = prenom
+
+    # Méthode pour retourner le nom et le prénom
     def SePresenter(self):
-        print("Je suis", + self.prenom + self.nom)
-        
-Personne1 = Personne("John", "Doe")
-Personne1.SePresenter()
+        return f"Je suis {self.prenom} {self.nom}."
+
+# Instanciation de plusieurs personnes
+personne1 = Personne("Doe", "John")
+personne2 = Personne("Dupond", "Jean")
+
+# Appel de la méthode SePresenter pour chaque personne
+print(personne1.SePresenter())
+print(personne2.SePresenter())
+
+
+"""
+Constructeur (__init__) : Le constructeur prend deux paramètres, nom et prenom,
+et les utilise pour initialiser les attributs de l'objet.
+
+Méthode SePresenter : Cette méthode retourne une chaîne de caractères qui présente la personne en utilisant
+les attributs nom et prenom.
+
+Instanciation : Nous créons trois instances de la classe Personne avec des valeurs différentes pour nom et
+prenom.
+
+Appel de la méthode SePresenter : Nous appelons la méthode SePresenter pour chaque instance et imprimons
+le résultat en console.
+
+"""
